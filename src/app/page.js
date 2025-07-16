1a3648ebb9cb8335c495f97e2036d86777ab1e5f
Gerardo Isaí Díaz <77507031+gerisai@users.noreply.github.com>
2025-07-16 12:53:49 -0600

    fix: clean up homepage (#6)
    Aprobado.

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,8 @@
 "use client"; 
 
 import React from 'react';
-import Button from '../components/Button.jsx'; 
 
 export default function Home() {
-  const handleClick = (buttonName) => {
-    alert(`¡Has clicado en el botón: ${buttonName}!`);
-  };
 
   return (
     <main className="min-h-screen flex items-center justify-center flex-col bg-white text-black p-8">
@@ -15,29 +11,6 @@ export default function Home() {
       <p className="text-lg text-center max-w-xl mb-8">
         Sitio en construcción. Aquí encontrarás próximamente información sobre nuestros proyectos, misión y formas de participar.
       </p>
-
-      {/* Container for buttons */}
-      <div className="flex flex-col md:flex-row gap-4 mt-8">
-        {/* First button: Súmate (primary) */}
-        <Button variant="primary" onClick={() => handleClick('Súmate')}>
-          Súmate
-        </Button>
-
-        {/* Second button: Súmate (secondary) */}
-        <Button variant="secondary" onClick={() => handleClick('Súmate')}>
-          Súmate
-        </Button>
-
-        {/* Third button: Conoce más (primary) */}
-        <Button variant="primary" onClick={() => handleClick('Conoce más')}>
-          Conoce más
-        </Button>
-
-        {/* four bottom: Conoce más (secondary) */}
-        <Button variant="secondary" onClick={() => handleClick('Conoce más')}>
-          Conoce más
-        </Button>
-      </div>
     </main>
   );
 }
