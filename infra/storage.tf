fe0dcb89876668a767969a5bcc69367505d5cef4
Gerardo Isaí Díaz <77507031+gerisai@users.noreply.github.com>
2025-07-27 08:58:32 -0600

    feat: create assets bucket (#11)

--- infra/storage.tf
+++ infra/storage.tf
@@ -13,3 +13,38 @@ resource "aws_s3_bucket_website_configuration" "website_config" {
     key = "error.html"
   }
 }
+
+resource "aws_s3_bucket" "media_assets" {
+  bucket = "${local.project_name}-media-assets"
+}
+
+resource "aws_s3_bucket_public_access_block" "media_assets" {
+  bucket = aws_s3_bucket.media_assets.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+data "aws_iam_policy_document" "allow_public_read" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.media_assets.arn}/*",
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_public_access" {
+  bucket = aws_s3_bucket.media_assets.id
+  policy = data.aws_iam_policy_document.allow_public_read.json
+}
