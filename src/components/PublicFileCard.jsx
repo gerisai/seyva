1f739175d4aaf919703c4cc5b8d77e3f95c7224f
Jair Hernandez Robles <jairhernandez904@gmail.com>
2025-08-04 18:54:01 -0600

    Feature/public file card (#14)
    * feat: Base colors and CSS styles
    
    * Public-File-Card
    
    * fix: public card component
    
    ---------
    
    Co-authored-by: Gerardo Diaz <gerardoisai.diaz@gmail.com>

---
+++ src/components/PublicFileCard.jsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import Image from 'next/image';
+
+const DEFAULT_ITEMS_TO_SHOW = 11;
+export default function PublicFileCard({
+  title,
+  icon,
+  items,
+  color,
+}) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const visibleItems = isExpanded ? items : items.slice(0, DEFAULT_ITEMS_TO_SHOW);
+
+  return (
+    <div className="w-fit rounded-4xl overflow-hidden shadow-lg border border-gray-200">
+      {/* Header */}
+      <div className={`flex justify-between items-center p-8 text-white bg-${color}`}>
+        <h2 className="text-5xl font-black">{title}</h2>
+        {icon && (
+          <Image src={icon} alt={`${title} icon`} width={80} height={80} />
+        )}
+      </div>
+
+      {/* Body */}
+      <div className={`bg-white p-8 overflow-hidden transition-all duration-300 ease-in-out`}>
+        <ul>
+          { visibleItems.map((item, index) => (
+            <li key={index} className="mb-2 text-black text-base">
+              <a href="#" className="underline hover:no-underline">
+                {item}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="p-8 pt-0 text-right">
+        <button onClick={toggleExpand} className={`text-lg text-${color} font-bold underline hover:no-underline`}>
+          { isExpanded ? 'Ver menos' : 'Ver más' }
+        </button>
+      </div>
+    </div>
+  );
+}
+
+PublicFileCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+  items: PropTypes.arrayOf(PropTypes.string).isRequired,
+  color: PropTypes.string, 
+};
