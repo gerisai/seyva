ab71bfad34c85bf146b2d3fdf491945db8d43d5f
Gerardo Diaz <gerardoisai.diaz@gmail.com>
2025-08-04 19:25:26 -0600

    fix: change domain to seyva.mx

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,7 +1,7 @@
 locals {
   project_name = "seyva"
   name         = "SEyVA"
-  domain_name  = "seyva.com.mx"
+  domain_name  = "seyva.mx"
   tags = {
     Environment = "Production"
     Project     = "seyva"
