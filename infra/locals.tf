d4b07e84c7cfbdc15d6e21a692a27fbd6d7d5cad
Gerardo Isaí Díaz <77507031+gerisai@users.noreply.github.com>
2025-08-08 07:34:13 -0600

    feat: create mail records for email forwarding to hostinger (#16)

--- infra/locals.tf
+++ infra/locals.tf
@@ -2,6 +2,48 @@ locals {
   project_name = "seyva"
   name         = "SEyVA"
   domain_name  = "seyva.mx"
+  mail_records = {
+    mx = {
+      name = ""
+      type = "MX"
+      ttl  = 14400
+      records = [
+        "5 mx1.hostinger.com",
+        "10 mx2.hostinger.com"
+      ]
+    },
+    spf = {
+      name    = ""
+      type    = "TXT"
+      ttl     = 3600
+      records = ["v=spf1 include:_spf.mail.hostinger.com ~all"]
+    },
+    dkim1 = {
+      name    = "hostingermail-a._domainkey"
+      type    = "CNAME"
+      ttl     = 300
+      records = ["hostingermail-a.dkim.mail.hostinger.com"]
+    },
+    dkim2 = {
+      name    = "hostingermail-b._domainkey"
+      type    = "CNAME"
+      ttl     = 300
+      records = ["hostingermail-b.dkim.mail.hostinger.com"]
+    },
+    dkim3 = {
+      name    = "hostingermail-c._domainkey"
+      type    = "CNAME"
+      ttl     = 300
+      records = ["hostingermail-c.dkim.mail.hostinger.com"]
+    },
+    auth = {
+      name    = "_dmarc"
+      type    = "TXT"
+      ttl     = 3600
+      records = ["v=DMARC1; p=none"]
+    }
+  }
+
   tags = {
     Environment = "Production"
     Project     = "seyva"
