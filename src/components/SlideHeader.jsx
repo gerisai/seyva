56fd02d2d746f27af542fc8cb69c79a5d98d159d
Gerardo Isaí Díaz <77507031+gerisai@users.noreply.github.com>
2025-08-10 00:07:47 -0600

    feat: add slide header (#18)

---
+++ src/components/SlideHeader.jsx
@@ -0,0 +1,58 @@
+"use client";
+
+export default function SlideHeader({ words, intervalTime = 3 }) {
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl md:text-6xl font-bold flex gap-3">
+        Vivir es
+        <span className="relative h-[1em] overflow-hidden inline-block">
+          <span className="animate-slide block">
+            { words.map((word) => (
+              <>
+              <span className={`text-${word.color}`}>{word.text}</span>
+              <br />
+              </>
+            ))}
+          </span>
+        </span>
+      </h1>
+
+      <style jsx>{`
+        @keyframes slide {
+          0% {
+            transform: translateY(0);
+          }
+          20% {
+            transform: translateY(0);
+          }
+          25% {
+            transform: translateY(-25%);
+          }
+          45% {
+            transform: translateY(-25%);
+          }
+          50% {
+            transform: translateY(-50%);
+          }
+          70% {
+            transform: translateY(-50%);
+          }
+          75% {
+            transform: translateY(-75%);
+          }
+          95% {
+            transform: translateY(-75%);
+          }
+          100% {
+            transform: translateY(0);
+          }
+        }
+
+        .animate-slide {
+          animation: slide ${intervalTime*words.length}s ease-in-out infinite;
+        }
+      `}</style>
+    </div>
+  );
+}
