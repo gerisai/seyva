0d942785e09e6c9676d8d32a97ff2dbe3e4d9f84
Gerardo Isaí Díaz <77507031+gerisai@users.noreply.github.com>
2025-08-11 07:35:47 -0600

    feat: add logo ribbon (#19)

---
+++ src/stories/LogoRibbon.stories.js
@@ -0,0 +1,14 @@
+import LogoRibbon from '@/components/LogoRibbon'
+
+export default {
+  title: 'Compound/LogoRibbon',
+  component: LogoRibbon,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs']
+};
+
+export const Main = {
+  args: {}
+};
