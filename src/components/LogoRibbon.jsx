4def13cb9b2a8ef2f130f3f7dcd888a75b97d486
Gerardo Diaz <gerardoisai.diaz@gmail.com>
2025-08-11 07:34:57 -0600

    feat: add logo ribbon

---
+++ src/components/LogoRibbon.jsx
@@ -0,0 +1,47 @@
+import Image from "next/image"
+
+const ribbon = [
+  { name: 'Salud', image: '/health.svg', color: 'green'},
+  { name: 'Educación', image: '/education.svg', color: 'orange'},
+  { name: 'Vivienda', image: '/house.svg', color: 'purple'},
+  { name: 'Alimentación', image: '/feed.svg', color: 'pink'}
+]
+
+export default function LogoRibbon() {
+  return (
+    <div className="w-full flex">
+      { ribbon.map((block) => (
+        <div className="w-1/4 justify-center items-center text-center overflow-hidden">
+          <div className={`h-4 bg-${block.color} ${ block.name === 'Salud' ? 'mb-6' : 'mb-4' }`}/>
+          <div className="animate-showfrombutton">
+            <Image
+              className="pt-6 px-8"
+              src={block.image}
+              alt={block.name}
+              width={150}
+              height={150}
+            />
+            <h1 className="mt-4 text-xl text-center font-bold text-gray-800">{block.name}</h1>
+          </div>
+        </div>
+      ))}
+    <style>{`
+    @keyframes showFromButton {
+      0% {
+        transform: translateY(100%);
+        opacity: 0;
+      }
+      100% {
+        transform: translateY(0);
+        opacity: 1;
+      }
+    }
+
+    .animate-showfrombutton {
+      animation: showFromButton 3s ease-in-out;
+    }
+    `}</style>
+    </div>
+
+  )
+}
