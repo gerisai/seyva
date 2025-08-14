a5057c7793708116e81951dcfd1880f456d8392a
Santiago Pichardo Robles <santiagopichardo@MacBook-Air-de-Santiago.local>
2025-08-13 20:49:45 -0600

    feat: Add Paragraph component and its stories

---
+++ src/components/Paragraph.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Paragraph({ title, content }) {
+  return (
+    <div className="flex flex-col max-w-2xl mx-auto p-4">
+      <h2 className="text-5xl font-bold text-blue mb-5">{title}</h2>
+      <p className="text-lg text-gray-800">{content}</p>
+    </div>
+  );
+}
+
+Paragraph.protoTypes = {
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+}
