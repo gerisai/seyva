01a7df11d8482a8839c08db831a63f0cdb6d5f90
Santiago Pichardo Robles <santiago.pichardo12r@gmail.com>
2025-08-15 08:17:13 -0600

    feat: Quote Component (#22)
    * feat: Quote Component
    
    * feat: Add Paragraph component and its stories
    
    * Fix: Corrected quote and paragraph
    
    * fix: paragraph
    
    ---------
    
    Co-authored-by: Santiago Pichardo Robles <santiagopichardo@MacBook-Air-de-Santiago.local>
    Co-authored-by: Gerardo Diaz <gerardoisai.diaz@gmail.com>

---
+++ src/stories/Quote.stories.js
@@ -0,0 +1,22 @@
+import Quote from "@/components/Quote";
+
+export default {
+  title: 'Components/Quote',
+  component: Quote,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs']
+};
+
+export const Main = {
+  args: {
+    quote:(
+        <>
+        La superación de la pobreza no es un<br />
+        gesto de caridad, es un acto de justicia.     
+        </>
+    ),
+    author:"Nelson Mandela"
+  }
+};
---
+++ src/stories/paragraph.stories.js
@@ -0,0 +1,53 @@
+import Paragraph from "@/components/Paragraph";
+import React from 'react'; 
+
+export default {
+  title: 'Components/Paragraph',
+  component: Paragraph,
+  tags: ['autodocs'],
+};
+
+export const Mision = {
+  args: {
+    title: "Mision",
+    content: (
+      <>
+        Contribuir al bienestar <strong>integral de personas y comunidades en situación<br />
+        de vulnerabilidad</strong>, brindando apoyo asistencial en salud, alimentación,<br />
+        vivienda, educación, orientación jurídica y desarrollo comunitario, con<br />
+        especial énfasis en los <strong>pueblos indígenas, afromexicanos y grupos<br />
+        históricamente marginados</strong>, a través de acciones humanitarias dignas,<br />
+        inclusivas y sostenibles.
+      </>
+    ),
+  },
+};
+
+export const Vision = {
+  args: {
+    title: "Visión",
+    content: (
+      <>
+        Ser una fundación referente a nivel nacional por su <strong>compromiso social,<br /></strong>
+        impacto tangible y cercanía con las comunidades que más lo necesitan,<br /> 
+        generando <strong>esperanza, equidad y desarrollo</strong> en cada intervención, con<br /> 
+        un enfoque incluyente y culturalmente sensible.
+      </>
+    ),
+  },
+};
+
+export const Objetivo = {
+  args: {
+    title: "Objetivo",
+    content: (
+      <>
+        Implementar y <strong>fortalecer programas asistenciales </strong>que atiendan<br /> 
+        necesidades básicas de subsistencia, salud, educación, trabajo, justicia y<br /> 
+        desarrollo comunitario, promoviendo la dignidad humana,<strong>la inclusión<br /> 
+        social y la mejora </strong>continua de la calidad de vida de los sectores más<br /> 
+        desfavorecidos.
+      </>
+    ),
+  },
+};
