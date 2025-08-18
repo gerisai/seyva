446f34f828723c39a4b5dbe97d9a69266da036e7
Gerardo Isaí Díaz <77507031+gerisai@users.noreply.github.com>
2025-08-18 08:56:03 -0600

    feat: add programs page (#33)

--- src/app/programs/page.js
+++ src/app/programs/page.js
@@ -1,13 +1,40 @@
 import Image from "next/image"
+import ProgramCard from "@/components/ProgramCard"
+import { baseAssetsUrl, programs } from "@/util/constants"
 
 export default function Home() {
   return (
-    <main className="h-lvh flex items-center justify-center flex-col bg-gray text-black">
-      <h1 className="text-4xl font-bold mb-4">Bienvenidos a Fundación Seyva</h1>
-      <p className="text-lg text-center max-w-xl mb-8">
-        Pagina en construcción. Aquí encontrarás próximamente información sobre nuestros proyectos, misión y formas de participar.
-      </p>
-      <Image alt='seyva-logo'src='/imagotipo.svg' width={250} height={250} />
+    <main className="flex flex-col bg-gray text-black">
+      <div className="absolute right-1/6 top-1/4 h-full">
+        <div className="absolute right-0 top-0 w-7 h-44 rounded-full bg-pink"></div>
+        <div className="absolute left-15 top-36 w-7 h-44 rounded-full bg-purple"></div>
+        <div className="absolute right-10 top-72 w-7 h-44 rounded-full bg-orange"></div>
+        <div className="absolute left-5 top-108 w-7 h-44 rounded-full bg-green"></div>
+      </div>
+      <div className="flex flex-col w-3/4 gap-16 py-10 px-26">
+        <div className="relative flex flex-col bg-green w-full h-64">
+          <Image alt='seyva-logo'src={`${baseAssetsUrl}/our-programs.png`} fill />
+        </div>
+        <h1 className="text-7xl font-bold text-gray">Nuestros programas</h1>
+        <p className="text-xl">
+          En nuestra fundación trabajamos cada día para transformar realidades.
+          Nuestros programas están diseñados para atender las necesidades más urgentes de las comunidades que acompañamos, 
+          con enfoque en salud, educación, vivienda y alimentación. Cada programa nace del contacto directo con las personas, 
+          de escuchar sus historias y entender sus retos.
+          <br/>
+          <br/>
+          No creemos en soluciones genéricas, sino en apoyos que verdaderamente cambien vidas. 
+          A través de estas iniciativas, buscamos no solo brindar ayuda inmediata, sino también construir bases firmes para un futuro más digno y justo.
+          <br/>
+          <br/>
+          Conoce cada uno de nuestros programas, su impacto y cómo puedes ser parte del cambio.
+        </p>
+      </div>
+      <div className="flex gap-16 py-12 px-12">
+        { programs.map((program,index) => (
+          <ProgramCard key={index} {...program} />
+        ))}
+      </div>
     </main>
   )
 }
