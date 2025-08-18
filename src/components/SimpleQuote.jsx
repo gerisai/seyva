aac519eb340e504366b5c32580b5f2797120d6a1
Jair Hernandez Robles <jairhernandez904@gmail.com>
2025-08-18 10:34:41 -0600

    SimpleQuote (#27)
    * SimpleQuote
    
    * fix: simple quote component
    
    * feat: add quote to stories
    
    ---------
    
    Co-authored-by: Gerardo Diaz <gerardoisai.diaz@gmail.com>

---
+++ src/components/SimpleQuote.jsx
@@ -0,0 +1,10 @@
+export default function SimpleQuote({ quote, author, colorClass }) {
+  return (
+    <div className="flex flex-col justify-center items-center py-10">
+      <div className="max-w-5xl px-6 relative">
+        <p className={`text-5xl ${colorClass}`}>{quote}</p>
+        <span className="text-5xl text-lightgray-simple  mt-2 block text-right">{author}</span>
+      </div>
+    </div>
+  );
+}
