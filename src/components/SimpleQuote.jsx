2e6c2276698cb485f05fa695d3afb98b95d5282e
Gerardo Diaz <gerardoisai.diaz@gmail.com>
2025-08-18 10:21:50 -0600

    fix: simple quote component

--- src/components/SimpleQuote.jsx
+++ src/components/SimpleQuote.jsx
@@ -2,8 +2,8 @@ export default function SimpleQuote({ quote, author, colorClass }) {
   return (
     <div className="flex flex-col justify-center items-center py-10">
       <div className="max-w-5xl px-6 relative">
-        <p className={`text-lg ${colorClass}`}>{quote}</p>
-        <span className="text-xl text-lightGray2 mt-2 block text-right">{author}</span>
+        <p className={`text-3xl ${colorClass}`}>{quote}</p>
+        <span className="text-3xl text-lightgray-simple  mt-2 block text-right">{author}</span>
       </div>
     </div>
   );
